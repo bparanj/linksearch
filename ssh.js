7fe5afc96c8005a7789efc017ac9cc6f2e6be2d3
Bala Paranj <bparanj@gmail.com>
2023-01-21 13:26:50 -0500

    Refactor SSH copy to clipboard code

---
+++ ssh.js
@@ -0,0 +1,44 @@
+import data from './ssh.json' assert {type: 'json'};
+
+window.dataTable = function () {
+  return {
+    items: data,
+    searchInput: '',
+    search(value) {
+      if (value.length > 1) {
+        const options = {
+          shouldSort: true,
+          keys: ['name']
+        }                
+        const fuse = new Fuse(data, options)
+        this.items = fuse.search(value).map(element => element.item)
+      } else {
+        this.items = data
+      }
+      // console.log(this.items.length)      
+    }
+  }
+}
+
+document.addEventListener('alpine:init', () => {
+
+	// Magic: $clipboard
+	Alpine.magic('clipboard', () => {
+	  return subject => navigator.clipboard.writeText(subject)
+	})
+
+  // Magic: $tooltip
+  Alpine.magic('tooltip', el => message => {
+
+    let instance = tippy(el, { content: message, trigger: 'manual' })
+
+    instance.show()
+
+    setTimeout(() => {
+      instance.hide()
+
+      setTimeout(() => instance.destroy(), 150)
+    }, 2000)
+  })
+
+})
